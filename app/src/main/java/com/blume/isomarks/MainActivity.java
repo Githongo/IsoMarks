ae7da061de1ba9f2bcb709d4584887a97fefb12d
Jeffrey Kingori <40299758+Githongo@users.noreply.github.com>
2020-01-08 00:02:23 +0300

    Sign up interface added

--- app/src/main/java/com/blume/isomarks/MainActivity.java
+++ app/src/main/java/com/blume/isomarks/MainActivity.java
@@ -2,13 +2,69 @@ package com.blume.isomarks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email, pword;
+    Button signin;
+    TextView pwordreset, signup;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.Email);
+        pword = findViewById(R.id.password);
+        signin = findViewById(R.id.login);
+        signup = findViewById(R.id.register);
+        pwordreset = findViewById(R.id.resetPassword);
+        progressBar = findViewById(R.id.progressBar1);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String e_mail = email.getText().toString();
+                String p_word = pword.getText().toString();
+
+                if(e_mail.isEmpty()){
+                    email.setError("Fill in email field");
+                    email.requestFocus();
+                }
+                else if(p_word.isEmpty()){
+                    pword.setError("Password field is empty");
+                    pword.requestFocus();
+                }
+                else if(p_word.isEmpty() && e_mail.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Fields are empty!!", Toast.LENGTH_SHORT).show();
+                }
+
+                else if (!(p_word.isEmpty() && e_mail.isEmpty())){
+                    // Put authentication code here.
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "An error occurred, Please try again in a few minutes...", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/blume/isomarks/RegisterActivity.java
@@ -0,0 +1,88 @@
+package com.blume.isomarks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText email, pword, username, phone, schoolID;
+    Button signup;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        email = findViewById(R.id.regEmail);
+        pword = findViewById(R.id.regPassword);
+        username = findViewById(R.id.username);
+        phone = findViewById(R.id.phone);
+        schoolID = findViewById(R.id.schoolID);
+        progressBar = findViewById(R.id.progressBar3);
+        signup = findViewById(R.id.regButton);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                final String u_name = username.getText().toString();
+                final String e_mail = email.getText().toString();
+                final String p_word = pword.getText().toString();
+                String uphone = phone.getText().toString();
+
+
+                final String staff_id= schoolID.getText().toString();
+
+
+                if(e_mail.isEmpty()){
+                    email.setError("Fill in email field");
+                    email.requestFocus();
+                }
+                else if(p_word.isEmpty()){
+                    pword.setError("Password field cannot be empty");
+                    pword.requestFocus();
+                }
+                else if(uphone.isEmpty()){
+                    phone.setError("Phone field cannot be empty");
+                    phone.requestFocus();
+                }
+
+                if(!(uphone.isEmpty())){
+                    if(!(uphone.length() == 10)){
+                        phone.setError("Phone number must be 10 digits");
+                        phone.requestFocus();
+                    }
+                    else {
+                        final int u_phone = Integer.parseInt(uphone);
+                    }
+                }
+                if(!(p_word.isEmpty())){
+                    if(p_word.length() < 6){
+                        pword.setError("Password must be at least 6 characters");
+                        pword.requestFocus();
+                    }
+
+                }
+
+
+                else if (!(p_word.isEmpty() && e_mail.isEmpty())){
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    //Put write new user to db code here...use u_name, e_mail, p_word, u_phone, staff_id.
+
+                }
+                else {
+                    Toast.makeText(RegisterActivity.this, "An error ccured, Please try again in a few...", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+}
