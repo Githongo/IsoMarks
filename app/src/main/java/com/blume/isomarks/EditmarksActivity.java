e3e1638d60b1a7278e48ccc558cb882b2d9e756b
Jeffrey Kingori <40299758+Githongo@users.noreply.github.com>
2020-01-24 23:28:04 +0300

    Edit marks UI updated

--- app/src/main/java/com/blume/isomarks/EditmarksActivity.java
+++ app/src/main/java/com/blume/isomarks/EditmarksActivity.java
@@ -7,18 +7,35 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.blume.edithelpers.EditAdapter;
+import com.blume.edithelpers.EditModel;
+
+import java.util.ArrayList;
+
 public class EditmarksActivity extends AppCompatActivity {
+    private ListView editLv;
+    private EditAdapter editAdapter;
+    public ArrayList<EditModel> editModelArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editmarks);
-
+        //initializing toolbar
         Toolbar myToolbar = findViewById(R.id.editmarks_toolbar);
         setSupportActionBar(myToolbar);
         setTitle(getIntent().getExtras().getString("subjectName")+", "+getIntent().getExtras().getString("stream"));
+
+        //initializing lv
+        editLv = (ListView) findViewById(R.id.editListView);
+
+        editModelArrayList = populateList();
+        editAdapter = new EditAdapter(this,editModelArrayList);
+        editLv.setAdapter(editAdapter);
+
     }
 
     //inflating toolbar with menu res
@@ -38,6 +55,24 @@ public class EditmarksActivity extends AppCompatActivity {
         if (itemId == R.id.editmarks_menu_sort_by_admno){
             Toast.makeText(EditmarksActivity.this, "Sorted...", Toast.LENGTH_SHORT).show();
         }
+        if (itemId == R.id.editmarks_menu_save){
+            String Mark = EditAdapter.editModelArrayList.get(0).getEditTextValue();
+            Toast.makeText(EditmarksActivity.this, Mark, Toast.LENGTH_SHORT).show();
+        }
         return super.onOptionsItemSelected(item);
     }
+
+    private ArrayList<EditModel> populateList() {
+        ArrayList<EditModel> list = new ArrayList<>();
+
+        for(int i = 0; i < 20; i++){
+            EditModel editModel = new EditModel();
+            editModel.setEditTextValue(String.valueOf(i));
+            list.add(editModel);
+        }
+
+        return list;
+    }
+
+
 }
