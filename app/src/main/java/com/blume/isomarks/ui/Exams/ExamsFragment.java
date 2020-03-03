44e5ef662f51313b0b9526e3846f1c37d2bb2f1b
Eltonomwega <notelgamwe@gmail.com>
2020-03-04 00:26:11 +0300

    exams page done[expected improvements on shimmer and date calculation]

--- app/src/main/java/com/blume/isomarks/ui/Exams/ExamsFragment.java
+++ app/src/main/java/com/blume/isomarks/ui/Exams/ExamsFragment.java
@@ -1,5 +1,7 @@
 package com.blume.isomarks.ui.Exams;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -8,23 +10,24 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.blume.isomarks.R;
 
@@ -33,6 +36,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ExamsFragment extends Fragment {
 
@@ -40,29 +47,35 @@ public class ExamsFragment extends Fragment {
     private ArrayList<String> mExamName = new ArrayList<>();
     private ArrayList<String> mFrom = new ArrayList<>();
     private ArrayList<String> mTo = new ArrayList<>();
-    private RequestQueue mRequestQueue;
+
+    private ArrayList<String> pExamName = new ArrayList<>();
+    private ArrayList<String> pFrom = new ArrayList<>();
+    private ArrayList<String> pTo = new ArrayList<>();
+
+    private RequestQueue mRequestQueue,queue;
     ExamsAdapter adapter;
-    RecyclerView recyclerView;
+    PastExamsAdapter pastadapter;
+    RecyclerView recyclerView,pastRecyclerview;
     ImageButton addButton;
+    com.google.android.material.button.MaterialButton addExam;
+    com.google.android.material.textfield.TextInputEditText mexamname,FromDate,ToDate;
+    Spinner ExamType, Term;
+    DatePickerDialog datePickerDialog;
     View root;
     Dialog myDialog;
+    //String ExamName,Examtype,term,mfrom,mto;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         root = inflater.inflate(R.layout.fragment_exams, container, false);
-        /*final TextView textView = root.findViewById(R.id.text_schools);
-        examsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
+
 
         getActivity().setTitle("Exams");
         myDialog = new Dialog(getContext());
 
 
         mRequestQueue = Volley.newRequestQueue(getContext());
+        queue = Volley.newRequestQueue(getContext());
 
         VolleyCallback volleyCallback=new VolleyCallback() {
             @Override
@@ -76,13 +89,34 @@ public class ExamsFragment extends Fragment {
                     e.printStackTrace();
                 }
             }
+
+            @Override
+            public void PastonSuccess(JSONObject jsonObject) {
+                try {
+                    pExamName.add((String)jsonObject.get("ExamName"));
+                    pFrom.add((String)jsonObject.get("fromDate"));
+                    pTo.add((String)jsonObject.get("toDate"));
+                    Log.d("volleyTest",Integer.toString(pExamName.size()));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
         };
         recyclerView = root.findViewById(R.id.examsList);
         adapter = new ExamsAdapter(getContext(),mExamName,mFrom,mTo);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
 
+
+        pastRecyclerview = root.findViewById(R.id.PastExams);
+        pastadapter = new PastExamsAdapter(getContext(),pExamName,pFrom,pTo);
+        pastRecyclerview.setAdapter(pastadapter);
+        pastRecyclerview.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false));
+
         fetchJsonResponse(volleyCallback);
+
+
+
 
         addButton = root.findViewById(R.id.addButton);
 
@@ -93,8 +127,47 @@ public class ExamsFragment extends Fragment {
 
                 myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 myDialog.show();
+
+
+                FromDate = myDialog.findViewById(R.id.FromD);
+                ToDate = myDialog.findViewById(R.id.ToD);
+                addExam = myDialog.findViewById(R.id.addExam);
+
+                mexamname = myDialog.findViewById(R.id.examName);
+                ExamType = myDialog.findViewById(R.id.examType);
+                Term = myDialog.findViewById(R.id.term);
+
+                //display the datepicker when field is clicked
+                FromDateDisplay();
+                ToDateDisplay();
+
+                addItemsOnExamType();
+                addItemsOnTerm();
+
+                addExam.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!mexamname.toString().equals("")&& !FromDate.toString().equals("")
+                        && !ToDate.toString().equals("")){
+                            postExam();
+                            AlertDialog alertDialog = new AlertDialog.Builder(getContext()).create();
+                            alertDialog.setMessage("Exam Posted successfully.");
+                            alertDialog.show();
+                            myDialog.cancel();
+                        }else {
+                            AlertDialog alertDialog = new AlertDialog.Builder(getContext()).create();
+                            alertDialog.setMessage("Please fill all the fields");
+                            alertDialog.show();
+
+                        }
+
+                    }
+                });
+
             }
         });
+
+
 
 
 
@@ -108,7 +181,10 @@ public class ExamsFragment extends Fragment {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
 
-
+        pastRecyclerview = root.findViewById(R.id.PastExams);
+        pastadapter = new PastExamsAdapter(getContext(),pExamName,pFrom,pTo);
+        pastRecyclerview.setAdapter(pastadapter);
+        pastRecyclerview.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false));
     }
 
 
@@ -125,7 +201,7 @@ public class ExamsFragment extends Fragment {
                         if((jsonObject.get("Status")).equals("1")){
                             volleyCallback.onSuccess(jsonObject);
                         }
-
+                        else {volleyCallback.PastonSuccess(jsonObject);}
 
 
 
@@ -153,7 +229,140 @@ public class ExamsFragment extends Fragment {
     public interface VolleyCallback{
 
         void onSuccess(JSONObject jsonObject);
+        void  PastonSuccess(JSONObject jsonObject);
     }
 
+    private void FromDateDisplay(){
+        FromDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
 
-}
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                FromDate.setText(year + "-"
+                                        + (monthOfYear + 1) + "-" + dayOfMonth);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+
+    }
+
+    private void ToDateDisplay() {
+        ToDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                ToDate.setText(year + "-"
+                                        + (monthOfYear + 1) + "-" + dayOfMonth);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+
+    }
+
+    public void addItemsOnExamType() {
+
+        ExamType = myDialog.findViewById(R.id.examType);
+        List<String> list = new ArrayList<String>();
+
+        list.add("Opener Exam");
+        list.add("Mid-Term Exam");
+        list.add("End Term Exam");
+        list.add("FORM 4");
+        list.add("JOINT EXAM");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getContext(),
+                R.layout.custom_spinner_items, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ExamType.setAdapter(dataAdapter);
+    }
+
+    public void addItemsOnTerm() {
+
+        Term = myDialog.findViewById(R.id.term);
+        List<String> list = new ArrayList<String>();
+
+        list.add("1");
+        list.add("2");
+        list.add("3");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getContext(),
+                R.layout.custom_spinner_items, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Term.setAdapter(dataAdapter);
+    }
+
+    public void postExam(){
+
+
+        String url = "http://kilishi.co.ke/Android_create_exam.php";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+
+                        Log.d("Response", response);
+
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        AlertDialog alertDialog = new AlertDialog.Builder(getContext()).create();
+                        alertDialog.setMessage("Your request timed out");
+                        alertDialog.show();
+                        // error
+                        //Log.d("Error.Response", response);
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Examname", mexamname.getText().toString());
+                params.put("From", FromDate.getText().toString());
+                params.put("To", ToDate.getText().toString());
+                params.put("ExamType", ExamType.getSelectedItem().toString());
+                params.put("Term", Term.getSelectedItem().toString());
+
+                return params;
+            }
+        };
+        queue.add(postRequest);
+
+
+    }
+
+    }
